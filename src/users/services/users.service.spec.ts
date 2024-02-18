41520444dcf4a87adc997f2aa8ec40b7eaeddfa2
mohamedeliwa <mohamed.av11@gmail.com>
2024-02-18 11:58:51 +0200

    solving linting issue

--- src/users/services/users.service.spec.ts
+++ src/users/services/users.service.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
