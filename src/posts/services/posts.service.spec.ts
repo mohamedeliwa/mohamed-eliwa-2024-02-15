fff5784fccd74b9313572ac3943b11e4edb91c35
mohamedeliwa <mohamed.av11@gmail.com>
2024-02-18 13:06:29 +0200

    added unit testing for create post method

--- src/posts/services/posts.service.spec.ts
+++ src/posts/services/posts.service.spec.ts
@@ -1,18 +1,70 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostsService } from './posts.service';
+import { Post } from '../entities/post.entity';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreatePostDto } from '../dtos/create.post.dto';
+import { User, UserRole } from '../../users/entities/user.entity';
 
 describe('PostsService', () => {
   let service: PostsService;
 
-  beforeEach(async () => {
+  let postsRepository: Repository<Post>;
+
+  const post_repository_token = getRepositoryToken(Post);
+
+  const user: User = {
+    id: 1,
+    username: 'username',
+    password: 'password',
+    role: UserRole.ADMIN,
+  };
+
+  const createPostDto: CreatePostDto = {
+    title: 'post title',
+    content: 'post conent',
+  };
+
+  const savedPost: Post = {
+    ...createPostDto,
+    id: 1,
+    author: user,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PostsService],
+      providers: [
+        PostsService,
+        {
+          provide: post_repository_token,
+          useValue: {
+            save: jest.fn(() => savedPost),
+            findOneOrFail: jest.fn(),
+            find: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<PostsService>(PostsService);
+    postsRepository = module.get<Repository<Post>>(post_repository_token);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('Create Post', () => {
+    it('should call postsRespository.save with right arguments', async () => {
+      await service.create(user, createPostDto);
+      expect(postsRepository.save).toHaveBeenCalledWith({
+        ...createPostDto,
+        author: user,
+      });
+    });
+
+    it('should return saved post', async () => {
+      const returnedPost = await service.create(user, createPostDto);
+      expect(returnedPost).toEqual(savedPost);
+    });
   });
 });
